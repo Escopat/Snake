2fe172857fca71a9d05ecb7a6d79f35de32547e3
Escopat <valery.kura@gmail.com>
2017-03-04 14:21:46 +0300

    Переместил Console.Readline(); как в уроке

--- Program.cs
+++ Program.cs
@@ -21,13 +21,14 @@ namespace Snake
             char sym2 = '#';
 
             Draw(x2, y2, sym2);
+
+            Console.ReadLine();
         }
 
         static void Draw(int x, int y, char sym)
         {
             Console.SetCursorPosition(x, y);
             Console.Write(sym);
-            Console.ReadLine();
         }
     }
 }
