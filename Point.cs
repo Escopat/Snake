a9cfb63beebee5426e466abfaf2505dcdee7efb0
Escopat <valery.kura@gmail.com>
2017-03-04 15:25:33 +0300

    Прмеры работы оперативной памяти

--- Point.cs
+++ Point.cs
@@ -14,8 +14,15 @@ namespace Snake
 
         public Point()
         {
-            Console.WriteLine("Создается новая точка");
         }
+
+        public Point(int _x, int _y, char _sym)
+        {
+            x = _x;
+            y = _y;
+            sym = _sym;
+        }
+
 
         public void Draw()
         {
